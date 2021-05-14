d4a59e66b7f4aab2f001b0b7776d876055a7c425
RahulParajulee <81411639+RahulParajulee@users.noreply.github.com>
2021-05-14 23:04:01 +0545

    completed code

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Library Management System</title>
+		<link href="stylesheet.css" rel="stylesheet" type="text/css" />
+	</head>
+
+	<body>
+   
+	   <div class="logo">
+		   <a><img src="images/Logo.png" width="125px" ></a>
+	   </div>
+	   <div class="col">
+		   <a href="admin.php" class="btn">Admin &#10137;</a>
+	   </div>
+	   <div class="col">
+		   <a href="student.php" class="btn">Student &#10137;</a>
+	   </div>
+   
+</body>
+</html>
